a287a416f84abdafe3d66adedd42ed506bc440b4
林明雄 <linmingxiong@pdmi.cn>
2018-05-17 09:31:21 +0800

    ~

--- client/api.js
+++ client/api.js
@@ -1,5 +1,6 @@
 const base64 = require('./utils/base64.js')
-const BASE_URL = 'https://app.toworks.cn/'
+const config = require('./config.js')
+const BASE_URL = config.service.host
 const USERNAME = 'ck_e56559a87557332cae32494540329002b793db82'
 const PASSWORD = 'cs_c978f6b0bab031fef539411344b0be8976c953c4'
 const Authorization = "Basic " + base64.encoder(USERNAME + ":" + PASSWORD)
@@ -7,7 +8,7 @@ function api(url, params = {}) {
   return new Promise((resolve, reject) => {
     // params.rest_route = url
     wx.request({
-      url: BASE_URL + 'wp-json' + url, //仅为示例，并非真实的接口地址
+      url: BASE_URL + '/wp-json' + url, //仅为示例，并非真实的接口地址
       data: params,
       header: {
         'content-type': 'application/json', // 默认值
--- client/config.js
+++ client/config.js
@@ -3,7 +3,7 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-var host = 'https://app.toworkds.cn';
+var host = 'https://app.bjgxht.cn';
 
 var config = {
 
